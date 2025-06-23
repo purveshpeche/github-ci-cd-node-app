187642fa97361a408da0b7c2a5e3b5fa24341b7e
purveshpech <purveshpeche6@gmail.com>
2025-06-23 13:27:59 +0000

    chnages in server,js file

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const port = 3000;
 
 const requestHandler = (req, res) => {
-  res.end('CI/CD with GitHub Actions is working! 🚀');
+  res.end('CI/CD with GitHub Actions is working! ');
 };
 
 const server = http.createServer(requestHandler);
