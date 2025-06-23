40261de711f39676986465fbc2e1d683a7f2f671
purveshpech <purveshpeche6@gmail.com>
2025-06-23 11:01:48 +0000

    Initial commit with CI/CD structure

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const port = 3000;
+
+const requestHandler = (req, res) => {
+  res.end('CI/CD with GitHub Actions is working! 🚀');
+};
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
